0dd2fba983f60ae71b7ab31c77e5dcd37df01eec
livochka <livochka@ucu.edu.ua>
2019-11-18 18:13:48 +0200

    Added Condition Ratings and web UI

--- airport/airport-pojo-bl/src/main/java/sofia/service/tablecodes/ConditionRating.java
+++ airport/airport-pojo-bl/src/main/java/sofia/service/tablecodes/ConditionRating.java
@@ -1,24 +1,19 @@
 package sofia.service.tablecodes;
 
-import sofia.validators.LongerThan2Validator;
-import sofia.validators.NoEmotionsValidator;
 import ua.com.fielden.platform.entity.AbstractPersistentEntity;
 import ua.com.fielden.platform.entity.DynamicEntityKey;
+import ua.com.fielden.platform.entity.annotation.KeyType;
+import ua.com.fielden.platform.entity.annotation.KeyTitle;
 import ua.com.fielden.platform.entity.annotation.CompanionObject;
 import ua.com.fielden.platform.entity.annotation.CompositeKeyMember;
-import ua.com.fielden.platform.entity.annotation.DescRequired;
-import ua.com.fielden.platform.entity.annotation.DescTitle;
-import ua.com.fielden.platform.entity.annotation.DisplayDescription;
-import ua.com.fielden.platform.entity.annotation.IsProperty;
-import ua.com.fielden.platform.entity.annotation.KeyTitle;
-import ua.com.fielden.platform.entity.annotation.KeyType;
 import ua.com.fielden.platform.entity.annotation.MapEntityTo;
 import ua.com.fielden.platform.entity.annotation.MapTo;
 import ua.com.fielden.platform.entity.annotation.Observable;
 import ua.com.fielden.platform.entity.annotation.Title;
-import ua.com.fielden.platform.entity.annotation.mutator.BeforeChange;
-import ua.com.fielden.platform.entity.annotation.mutator.Handler;
-import ua.com.fielden.platform.entity.annotation.mutator.IntParam;
+import ua.com.fielden.platform.entity.annotation.DescTitle;
+import ua.com.fielden.platform.entity.annotation.DisplayDescription;
+import ua.com.fielden.platform.entity.annotation.IsProperty;
+import ua.com.fielden.platform.entity.annotation.DescRequired;
 import ua.com.fielden.platform.reflection.TitlesDescsGetter;
 import ua.com.fielden.platform.utils.Pair;
 
@@ -29,15 +24,15 @@ import ua.com.fielden.platform.utils.Pair;
  *
  */
 @KeyType(DynamicEntityKey.class)
-@KeyTitle("Service Status")
-@CompanionObject(IServiceStatus.class)
+@KeyTitle("Condition Rating")
+@CompanionObject(IConditionRating.class)
 @MapEntityTo
 @DescTitle("Description")
 @DisplayDescription
 @DescRequired
-public class ServiceStatus extends AbstractPersistentEntity<DynamicEntityKey> {
+public class ConditionRating extends AbstractPersistentEntity<DynamicEntityKey> {
 
-    private static final Pair<String, String> entityTitleAndDesc = TitlesDescsGetter.getEntityTitleAndDesc(ServiceStatus.class);
+    private static final Pair<String, String> entityTitleAndDesc = TitlesDescsGetter.getEntityTitleAndDesc(ConditionRating.class);
     public static final String ENTITY_TITLE = entityTitleAndDesc.getKey();
     public static final String ENTITY_DESC = entityTitleAndDesc.getValue();
     
@@ -45,13 +40,10 @@ public class ServiceStatus extends AbstractPersistentEntity<DynamicEntityKey> {
     @MapTo
     @Title(value = "Name", desc = "Desc")
     @CompositeKeyMember(1)
-    @BeforeChange({
-        @Handler(NoEmotionsValidator.class), 
-        @Handler(value = LongerThan2Validator.class, integer = @IntParam(name = "minLength", value=3))})
     private String name;
-
+    
     @Observable
-    public ServiceStatus setName(final String name) {
+    public ConditionRating setName(final String name) {
         this.name = name;
         return this;
     }
@@ -62,11 +54,9 @@ public class ServiceStatus extends AbstractPersistentEntity<DynamicEntityKey> {
     
     @Override
     @Observable
-    public ServiceStatus setDesc(String desc) {
+    public ConditionRating setDesc(String desc) {
         super.setDesc(desc);
         return this;
     }
-    
-
 
 }
