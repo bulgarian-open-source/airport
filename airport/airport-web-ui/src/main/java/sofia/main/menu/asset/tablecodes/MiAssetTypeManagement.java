f0e38096b1ce5dc6d6b3010f8f5533970a2a3619
livochka <livochka@ucu.edu.ua>
2019-12-21 15:52:21 +0200

    added AssetTypeManagement UI

---
+++ airport/airport-web-ui/src/main/java/sofia/main/menu/asset/tablecodes/MiAssetTypeManagement.java
@@ -0,0 +1,15 @@
+package sofia.main.menu.asset.tablecodes;
+
+import ua.com.fielden.platform.entity.annotation.EntityType;
+import ua.com.fielden.platform.ui.menu.MiWithConfigurationSupport;
+import sofia.asset.tablecodes.AssetTypeManagement;
+/**
+ * Main menu item representing an entity centre for {@link AssetTypeManagement}.
+ *
+ * @author Developers
+ *
+ */
+@EntityType(AssetTypeManagement.class)
+public class MiAssetTypeManagement extends MiWithConfigurationSupport<AssetTypeManagement> {
+
+}
